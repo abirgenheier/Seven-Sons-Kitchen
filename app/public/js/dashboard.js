4277fd8e3eeb4a87d30d581161ae6e3f7f285544
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-06-22 12:39:54 -0700

    updated backend UI/DB connection. Adding more UI effects for search/nav bar

--- app/public/js/dashboard.js
+++ app/public/js/dashboard.js
@@ -24,7 +24,7 @@ $(document).ready(() => {
                             <p class="order-body" id="order">${order.order}</p>
                         <footer class="blockquote-footer">
                                 <p class="order_name">${order.name}</p>
-                                <p class="phone_number">${order.phone_number}</p>
+                                <p class="phone_number">${order.phone_number == '' ? '-No Phone Number Present-' : order.phone_number}</p>
                         </footer>
                         </blockquote>
                     </div>
@@ -42,7 +42,7 @@ function finished() {
         console.log(event.target.classList)
         setTimeout(() => {
             $(`#${_id}`).empty()
-        }, 400)
+        }, 600)
         var complete = {
             complete: 1
         }
@@ -57,18 +57,51 @@ function finished() {
             }
         );
     }
-
 }
 
-var fifteen_minutes = 15 * 60 * 1000;
-$(document).ready(() => {
-    now = time.split(':')
-    console.log(now)
+function template(a, b) {
+    a.innerHTML += `         Due at <b>${b}</b>`
+    a.parentNode.classList = ""
+    a.parentNode.classList.add('order_time')
+}
 
+$(document).ready(() => {
+    setTimeout(() => {
+        now = time.split(':')
+        console.log(now)
+        NodeList.prototype.forEach = Array.prototype.forEach;
+        var nodes = document.querySelectorAll('p.order_time').forEach(div_time => {
+            _time = (div_time.id).split(":")
+            var plus_fifteen = (+_time[1] + 15)
+            var deadline = (_time[0] + ":" + plus_fifteen)
+            if (_time[0] === now[0]) {
+                if (now[1] - +_time[1] >= 0 && now[1] - +_time[1] <= 10) {
+                    template(div_time, deadline)
+                    div_time.parentNode.classList.add('green')
+                } else if (now[1] - +_time[1] >= 11 && now[1] - +_time[1] <= 14) {
+                    template(div_time, deadline)
+                    div_time.parentNode.classList.add('yellow')
+                } else if (now[1] - +_time[1] >= 15) {
+                    template(div_time, deadline)
+                    div_time.parentNode.classList.add('red')
+                }
+            } else {
+                div_time.style.textDecoration = 'line-through'
+            }
+        })
+    }, 200)
 })
 
-$(document).click(() => {
-    (document.querySelectorAll('p.order_time')).map(item => {
-        console.log(item.id)
-    })
+$('.search').click(() => {
+    $('.search').attr("placeholder", "Order:")
+})
+
+$('.search').one("click", () => {
+    console.log('sd')
+    document.querySelector('.search').textContent += "Order:"
+})
+$('.search').keydown((e) => {
+    text = String.fromCharCode(e.which)
+    console.log(text)
+    document.querySelector('.search').innerHTML += text
 })
